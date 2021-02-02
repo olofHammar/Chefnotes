320c8fa9b52da57406f613bab7fe3ee2dbe2e4cf
Olof Hammar <olle_hammat@hotmail.com>
2021-02-02 22:12:29 +0100

    Initial Commit

---
+++ Chefnotes/ChefnotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChefnotesApp.swift
+//  Chefnotes
+//
+//  Created by Olof Hammar on 2021-02-02.
+//
+
+import SwiftUI
+
+@main
+struct ChefnotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
