50d07b57360f5ad60215aa19c8cc23ed1a9a5a20
Olof Hammar <olle_hammat@hotmail.com>
2021-02-10 19:08:24 +0100

    Can now get recipes from firestore. Seems to work fine. Also sorts in to categories.

--- Chefnotes/ReusableViews/RecipeBrowseView.swift
+++ Chefnotes/ReusableViews/RecipeBrowseView.swift
@@ -6,62 +6,90 @@
 //
 
 import SwiftUI
+import Firebase
 
 struct RecipeBrowseView: View {
     
-    var favorite: FavoriteRecipe
-
+    @State var recipeImage: UIImage?
+    var recipe: RecipePost
+    var postHeight: CGFloat = 320
+    
     var body: some View {
-        VStack {
-            ZStack{
-            favorite.image
-                .resizable()
-                .scaledToFill()
-                .frame(width: UIScreen.main.bounds.size.width-50, height: 270)
-                .cornerRadius(5)
-            Text(favorite.title)
+        ZStack {
+                Image("default_image")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: UIScreen.main.bounds.width, height: postHeight)
+                    .clipped()
+                    .background(Color.init(red: 0.9, green: 0.9, blue: 0.9))
+                LinearGradient(gradient: Gradient(colors: [Color.clear, Color.clear, Color.black]), startPoint: .top, endPoint: .bottom)
+            
+            HStack {
                 
-                .font(.title3)
-                .fontWeight(.semibold)
-                .frame(width: UIScreen.main.bounds.size.width-50, height: 60)
-                .background(Color.white).opacity(0.9)
-                .foregroundColor(.black)
-                .multilineTextAlignment(.center)
-                .padding(.top, 210)
-                .padding(.horizontal, 12)
-            }
-            
-            HStack{
-            VStack {
-                HStack{
-                    Image(systemName: "timer")
-                        .resizable()
-                        .frame(height: 18)
-                        .scaledToFit()
-            Text("Cookingtime: 30 min")
-                .font(.subheadline)
-                }
-                HStack{
-                    Image(systemName: "book")
-                        .resizable()
-                        .frame(height: 18)
-                        .scaledToFit()
-            Text("Author: Olle Hammar")
-                .font(.subheadline)
-            }
-            }.padding(.bottom)
                 Spacer()
-            }.padding(.leading)
-            .padding(.top, 10)
-            
-        }.frame(width: UIScreen.main.bounds.size.width-50, height: 353)
-        .overlay(RoundedRectangle(cornerRadius: 12).stroke(lineWidth: 0.5))
-        .background(Color.white)
+                
+                Text(recipe.title)
+                    .font(.title2)
+                    .fontWeight(.semibold)
+                    .padding(.leading)
+                    .padding(.trailing)
+                
+                Spacer()
+                
+                Image(systemName: "ellipsis.circle")
+                    .resizable()
+                    .frame(width: 25, height: 25)
+                    .padding(.trailing)
+                    .foregroundColor(.blue)
+                
+            }
+            .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height/11)
+            .background(Color.white).opacity(0.9)
+            .padding(.bottom, 253)
+
+            VStack {
+                Spacer()
+                HStack{
+                    VStack(alignment: .leading){
+                        HStack {
+                            Image(systemName: "book")
+                            Text("Author:")
+                            Text(recipe.author)
+                        }
+                        .padding(.bottom, 2)
+                        HStack {
+                            Image(systemName: "timer")
+                            Text("Cookingtime: ")
+                            Text("30 min")
+                        }
+                    }
+                    .foregroundColor(.white)
+                    .padding()
+                    
+                    Spacer()
+                }
+                .font(.system(size: 15, weight: .bold))
+            }
+        }.onAppear() {
+            getImage()
+        }
+        .frame(width: UIScreen.main.bounds.width, height: postHeight)
+    }
+    
+    private func getImage() {
+        let Ref = Storage.storage().reference(forURL: recipe.image)
+        Ref.getData(maxSize: 1 * 1024 * 1024) { data, error in
+            if error != nil {
+                print("Error: Image could not download!")
+            } else {
+                self.recipeImage = UIImage(data: data!)!
+            }
+        }
     }
 }
 
 struct RecipeBrowseView_Previews: PreviewProvider {
     static var previews: some View {
-        RecipeBrowseView(favorite: FavoriteRecipe(image: Image("steak-1"), title: "Steak with butter and rosemary"))
+        RecipeBrowseView(recipe: RecipePost(title: "", steps: [], ingredients: [], serves: 1, author: "", authorId: "", category: "", image: ""))
     }
 }
